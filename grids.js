eebdab7d1b40f2dcddec7969494555f85c407e59
1995eaton <1995eaton@gmail.com>
2014-05-15 12:12:50 -0400

    updates

--- grids.js
+++ grids.js
@@ -1,61 +1,61 @@
 Grid.presets = {
-  glidergun: [["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                          *                       "],
-              ["                        * *                       "],
-              ["              **      **            **            "],
-              ["             *   *    **            **            "],
-              ["  **        *     *   **                          "],
-              ["  **        *   * **    * *                       "],
-              ["            *     *       *                       "],
-              ["             *   *                                "],
-              ["              **                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "],
-              ["                                                  "]]
+  glidergun: ["                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                          *                       ",
+              "                        * *                       ",
+              "              **      **            **            ",
+              "             *   *    **            **            ",
+              "  **        *     *   **                          ",
+              "  **        *   * **    * *                       ",
+              "            *     *       *                       ",
+              "             *   *                                ",
+              "              **                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  ",
+              "                                                  "]
 };
 
 Grid.applyPreset = function(preset) {
   Grid.resetGrid();
   Grid.context.beginPath();
   for (var y = 0; y < this.height; ++y) {
-    var row = this.presets[preset][y][0].split("");
+    var row = this.presets[preset][y % (this.presets[preset].length - 1)].split("");
     for (var x = 0; x < this.width; ++x) {
       if (row[x] === "*") {
         this.pixels[y][x] = true;
@@ -63,6 +63,29 @@ Grid.applyPreset = function(preset) {
       }
     }
   }
+  Grid.context.closePath();
+  Grid.context.fill();
+};
+
+Grid.createRandom = function(s) {
+  Grid.resetGrid();
+  Grid.context.beginPath();
+  delete this.presets.random;
+  Grid.presets.random = [];
+  for (var y = 0; y < this.height; ++y) {
+    var string = "";
+    for (var x = 0; x < this.width; ++x) {
+      if (Math.floor(Math.random() * s) === 0) {
+        this.pixels[y][x] = true;
+        string += "*";
+        Grid.putPixel(x, y);
+      } else {
+        string += " ";
+      }
+    }
+    this.presets.random.push(string);
+  }
+  log("[\"" + this.presets.random.join("\",\n \"") + "\"]");
   Grid.context.closePath();
   Grid.context.fill();
 };
